a4ac8af9d86c660984086692b7eb342838309dbb
mona3 <monahassan763@gmail.com>
2016-05-03 10:02:56 +0200

    m

--- final/Product_1.php
+++ final/Product_1.php
@@ -25,7 +25,7 @@ $Username = $controller->get_UserName();
 
     <!-- Custom CSS -->
     <link href="css/shop-item.css" rel="stylesheet">
-
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -45,38 +45,12 @@ $Username = $controller->get_UserName();
         }
     </style>
  
- <style>
+ 
 
-
-* {
-  margin: 0;
-  padding: 0;
-  font-family: roboto;
-}
+<style>
 
 
 
-.cont {
-  width: 100%;
-  max-width: 350px;
-  text-align: center;
-  background: #fff;
-  color: #EEE;
-  overflow: hidden;
-}
-
-hr {
-  margin: 20px;
-  border: none;
-
-}
-
-div.title { font-size: 2em; }
-
-h1 span {
-  font-weight: 300;
-  color: #Fd4;
-}
 
 div.stars {
   width: 270px;
@@ -89,13 +63,13 @@ label.star {
   float: right;
   padding: 10px;
   font-size: 36px;
-  color: #444;
+  color: #FD4;
   transition: all .2s;
 }
 
 input.star:checked ~ label.star:before {
   content: '\f005';
-  color: #FD4;
+  color: FE7;
   transition: all .25s;
 }
 
@@ -114,8 +88,9 @@ label.star:before {
 }
 </style>
 
-
 </head>
+
+
 
 <body>
 
@@ -204,35 +179,20 @@ $controller->Add_to_cart();
 						<p><?php echo "Country:  ".$row['Country'];?></p>
 							  <p><font size="5" color="red"><?php echo "Description: ";?></font></p>
 				<p><font size="3" color="black"><?php echo $row["Description"];?></font></p>
-                 <?php 
-  
-  $controller->Calc_Rate();
-  ?><p>
-                            <?php echo "".$row['Rate'];?><?php echo "  Stars ";?>
-							
-							<div class="container">
-<div class="cont" style="margin-top:150px;">
-<div class="cont">
+                <div class="cont">
   <div class="stars">
-    <form action="" method="post">
-      <input class="star star-5" id="star-5-2" type="radio" name="star" value="5"/>
+    <form action="">
+      <input class="star star-5" id="star-5-2" type="radio" name="star"/>
       <label class="star star-5" for="star-5-2"></label>
-      <input class="star star-4" id="star-4-2" type="radio" name="star" value="4"/>
-      <label class="star star-4" for="star-4-2"></label>
-      <input class="star star-3" id="star-3-2" type="radio" name="star" value="3"/>
+      <input class="star star-3" id="star-3-2" type="radio" name="star"/>
       <label class="star star-3" for="star-3-2"></label>
-      <input class="star star-2" id="star-2-2" type="radio" name="star" value="2"/>
+      <input class="star star-2" id="star-2-2" type="radio" name="star"/>
       <label class="star star-2" for="star-2-2"></label>
-      <input class="star star-1" id="star-1-2" type="radio" name="star" value="1"/>
+      <input class="star star-1" id="star-1-2" type="radio" name="star"/>
       <label class="star star-1" for="star-1-2"></label>
-      
-	   </div>
-<Input name="action" type="submit" value="submit_rate">
     </form>
- 
   </div>
-  </div>
-  </div>
+</div>
                     </div>
             </div>
 
@@ -306,6 +266,9 @@ $controller->Add_to_cart();
 
     </div>
     <!-- /.container -->
+
+
+
 
     <div class="container">
 
